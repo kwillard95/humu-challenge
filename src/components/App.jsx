ec715fdc3658147b064e55d700ac1beba9b85d99
kwillard95 <kwillard95@gmail.com>
2020-02-18 10:46:53 -0800

    Placed logo and nav bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import employeeData from '../../employees.js'
+import Styles from '../styles/AppStyle.js'
 
 export default function App() {
-    return <div>Hello</div>
+    return (
+        <div>
+            <div style={{ display: 'block', marginLeft: '100px', marginTop: '20px', marginBottom: '20px', marginRight: '100px' }}>
+                <Styles.LogoContainer>
+                    <Styles.Logo src="./assets/Zero.png" />
+                </Styles.LogoContainer>
+                
+                <Styles.Nav>FAQs</Styles.Nav> <Styles.Nav>About</Styles.Nav> 
+            </div>
+            <Styles.Divider />
+        </div>
+    )
 }
