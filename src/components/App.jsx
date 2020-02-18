5a72ab431631329b22d24234204ba9786e411c74
kwillard95 <kwillard95@gmail.com>
2020-02-18 11:57:49 -0800

    Restructured navbar into seperate component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import employeeData from '../../employees.js'
-import Styles from '../styles/AppStyle.js'
+import NavBar from './NavBar.jsx'
 
 export default function App() {
     return (
         <div>
-            <div style={{ display: 'block', marginLeft: '100px', marginTop: '20px', marginBottom: '20px', marginRight: '100px' }}>
-                <Styles.LogoContainer>
-                    <Styles.Logo src="./assets/Zero.png" />
-                </Styles.LogoContainer>
-                
-                <Styles.Nav>FAQs</Styles.Nav> <Styles.Nav>About</Styles.Nav> 
-            </div>
-            <Styles.Divider />
+            <NavBar/>
         </div>
     )
 }
